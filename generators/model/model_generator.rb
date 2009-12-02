edb3ef1e32d1f417d35b327745468924f983e753
ZhangJinzhu <wosmvp@gmail.com>
2009-12-02 19:43:17 +0800

    add model generators

---
+++ generators/model/model_generator.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'active_record'
+
+class ModelGenerator < RubiGen::Base
+  attr_reader :name,:class_name,:file_name,:class_path,:table_name,:attributes
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @destination_root = File.expand_path('.')
+    @name             = args.shift
+		@attributes				= []
+
+		args.map do |x|
+			@attributes << {:type => x.split(/:/)[1],:name => x.split(/:/)[0]}
+		end
+
+    @file_name  = @name.pluralize.underscore
+    @class_name = @name.singularize.classify
+    @class_path = File.dirname(@file_name)
+		@table_name = file_name.gsub(/\//, '_').pluralize
+  end
+
+  def manifest
+    record do |m|
+      m.directory File.join("test/app_root/app/models",class_path)
+
+      m.template 'model.rb',File.join("test/app_root", 'app/models',"#{file_name}.rb")
+      # Migration
+      m.migration_template 'migration.rb', "test/app_root/db/migrate", :assigns => {
+        :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}"
+      }, :migration_file_name => "create_#{@table_name}"
+    end
+  end
+end
